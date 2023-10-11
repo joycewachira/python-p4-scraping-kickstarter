435b1959176385048ff57d9610e10029814c089c
Joyce <joycewachira718@gmail.com>
2023-10-11 20:31:13 +0300

    Completed lab

--- lib/kickstarter_scraper.py
+++ lib/kickstarter_scraper.py
@@ -1,31 +1,38 @@
 from bs4 import BeautifulSoup
 import ipdb
 
-# projects: kickstarter.select("li.project.grid_4")[0]
-# title: project.select("h2.bbcard_name strong a")[0].text
-# image link: project.select("div.project-thumbnail a img")[0]['src']
-# description: project.select("p.bbcard_blurb")[0].text
-# location: project.select("ul.project-meta span.location-name")[0].text
-# percent_funded: project.select("ul.project-stats li.first.funded strong")[0].text.replace("%","")
-
 def create_project_dict():
     html = ''
     with open('./fixtures/kickstarter.html') as file:
         html = file.read()
+
     kickstarter = BeautifulSoup(html, 'html.parser')
     projects = {}
+
     # Iterate through the projects
     for project in kickstarter.select("li.project.grid_4"):
         title = project.select("h2.bbcard_name strong a")[0].text
-        projects[title] = {
-        'image_link': project.select("div.project-thumbnail a img")[0]["src"],
-        'description': project.select("p.bbcard_blurb")[0].text,
-        'location': project.select("ul.project-meta span.location-name")[0].text,
-        'percent_funded': project.select("ul.project-stats li.first.funded strong")[0].text.replace("%","")
-        }
-    # return the projects dictionary
+        image_link = project.select("div.project-thumbnail a img")[0]['src']
+        description = project.select("p.bbcard_blurb")[0].text
+        location = project.select("ul.project-meta span.location-name")[0].text
+        percent_funded = project.select("ul.project-stats li.first.funded strong")[0].text.replace("%", "")
 
+        # Create a dictionary for each project and store it in the 'projects' dictionary
+        projects[title] = {
+            'image_link': image_link,
+            'description': description,
+            'location': location,
+            'percent_funded': percent_funded
+        }
+
+    # Return the 'projects' dictionary
     return projects
 
-projects = create_project_dict()
-print(projects)
+if __name__ == "__main__":
+    scraped_projects = create_project_dict()
+    for title, project_data in scraped_projects.items():
+        print(f"Title: {title}")
+        print(f"Image Link: {project_data['image_link']}")
+        print(f"Description: {project_data['description']}")
+        print(f"Location: {project_data['location']}")
+        print(f"Percent Funded: {project_data['percent_funded']}%\n")
